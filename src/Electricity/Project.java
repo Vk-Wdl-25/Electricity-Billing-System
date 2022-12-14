cefa4bac2fda79bc398ac91192ece95e05829d39
Vivek Wadile <viveks.realme@gmail.com>
2022-12-14 22:31:24 +0530

    fourthd commit

--- src/Electricity/Project.java
+++ src/Electricity/Project.java
@@ -17,7 +17,7 @@ public class Project extends JFrame implements ActionListener{
         setVisible(false);
         
         /* Adding background image */
-        ImageIcon ic =  new ImageIcon(ClassLoader.getSystemResource("icon/elec.png"));
+        ImageIcon ic =  new ImageIcon(ClassLoader.getSystemResource("icon/elect1.jpg"));
         Image i3 = ic.getImage().getScaledInstance(1500, 740,Image.SCALE_DEFAULT);
         ImageIcon icc3 = new ImageIcon(i3);
         JLabel l1 = new JLabel(icc3);
